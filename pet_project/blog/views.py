b395dc9aa96e4806e0cc9638ede93b5df17f4a96
Daniil Kimstach <daniilkimstachp@gmail.com>
2022-07-16 09:32:33 +0300

    Search was updated.

--- pet_project/blog/views.py
+++ pet_project/blog/views.py
@@ -61,7 +61,7 @@ def search_post(request):
     search_form = Search_Post_Form(request.POST)
     title = search_form.data['search_post']
     try:
-        post = Post.objects.filter(title=title)
+        post = Post.objects.filter(title__contains=str(title))
     except:
         post = ''
     if post:
